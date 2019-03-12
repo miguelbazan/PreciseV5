78fd28e061b6227e6df907424ee96480f863a7b5
Miguel Bazán <miguelb1995@icloud.com>
2019-03-11 22:06:09 -0600

    Lexer y parser listos (falta manejo de error de sintaxis)

---
+++ Grammar/PreciseV5Listener.java
@@ -0,0 +1,189 @@
+// Generated from PreciseV5.g4 by ANTLR 4.7.1
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link PreciseV5Parser}.
+ */
+public interface PreciseV5Listener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#preciseV5}.
+	 * @param ctx the parse tree
+	 */
+	void enterPreciseV5(PreciseV5Parser.PreciseV5Context ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#preciseV5}.
+	 * @param ctx the parse tree
+	 */
+	void exitPreciseV5(PreciseV5Parser.PreciseV5Context ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#declare}.
+	 * @param ctx the parse tree
+	 */
+	void enterDeclare(PreciseV5Parser.DeclareContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#declare}.
+	 * @param ctx the parse tree
+	 */
+	void exitDeclare(PreciseV5Parser.DeclareContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#type}.
+	 * @param ctx the parse tree
+	 */
+	void enterType(PreciseV5Parser.TypeContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#type}.
+	 * @param ctx the parse tree
+	 */
+	void exitType(PreciseV5Parser.TypeContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#array}.
+	 * @param ctx the parse tree
+	 */
+	void enterArray(PreciseV5Parser.ArrayContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#array}.
+	 * @param ctx the parse tree
+	 */
+	void exitArray(PreciseV5Parser.ArrayContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#body}.
+	 * @param ctx the parse tree
+	 */
+	void enterBody(PreciseV5Parser.BodyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#body}.
+	 * @param ctx the parse tree
+	 */
+	void exitBody(PreciseV5Parser.BodyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#estatuto}.
+	 * @param ctx the parse tree
+	 */
+	void enterEstatuto(PreciseV5Parser.EstatutoContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#estatuto}.
+	 * @param ctx the parse tree
+	 */
+	void exitEstatuto(PreciseV5Parser.EstatutoContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#expresionbool}.
+	 * @param ctx the parse tree
+	 */
+	void enterExpresionbool(PreciseV5Parser.ExpresionboolContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#expresionbool}.
+	 * @param ctx the parse tree
+	 */
+	void exitExpresionbool(PreciseV5Parser.ExpresionboolContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#function}.
+	 * @param ctx the parse tree
+	 */
+	void enterFunction(PreciseV5Parser.FunctionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#function}.
+	 * @param ctx the parse tree
+	 */
+	void exitFunction(PreciseV5Parser.FunctionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#expresion}.
+	 * @param ctx the parse tree
+	 */
+	void enterExpresion(PreciseV5Parser.ExpresionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#expresion}.
+	 * @param ctx the parse tree
+	 */
+	void exitExpresion(PreciseV5Parser.ExpresionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#exp}.
+	 * @param ctx the parse tree
+	 */
+	void enterExp(PreciseV5Parser.ExpContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#exp}.
+	 * @param ctx the parse tree
+	 */
+	void exitExp(PreciseV5Parser.ExpContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#termino}.
+	 * @param ctx the parse tree
+	 */
+	void enterTermino(PreciseV5Parser.TerminoContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#termino}.
+	 * @param ctx the parse tree
+	 */
+	void exitTermino(PreciseV5Parser.TerminoContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#factor}.
+	 * @param ctx the parse tree
+	 */
+	void enterFactor(PreciseV5Parser.FactorContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#factor}.
+	 * @param ctx the parse tree
+	 */
+	void exitFactor(PreciseV5Parser.FactorContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#condicion}.
+	 * @param ctx the parse tree
+	 */
+	void enterCondicion(PreciseV5Parser.CondicionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#condicion}.
+	 * @param ctx the parse tree
+	 */
+	void exitCondicion(PreciseV5Parser.CondicionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#ciclo}.
+	 * @param ctx the parse tree
+	 */
+	void enterCiclo(PreciseV5Parser.CicloContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#ciclo}.
+	 * @param ctx the parse tree
+	 */
+	void exitCiclo(PreciseV5Parser.CicloContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#escritura}.
+	 * @param ctx the parse tree
+	 */
+	void enterEscritura(PreciseV5Parser.EscrituraContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#escritura}.
+	 * @param ctx the parse tree
+	 */
+	void exitEscritura(PreciseV5Parser.EscrituraContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#lectura}.
+	 * @param ctx the parse tree
+	 */
+	void enterLectura(PreciseV5Parser.LecturaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#lectura}.
+	 * @param ctx the parse tree
+	 */
+	void exitLectura(PreciseV5Parser.LecturaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#asignacion}.
+	 * @param ctx the parse tree
+	 */
+	void enterAsignacion(PreciseV5Parser.AsignacionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#asignacion}.
+	 * @param ctx the parse tree
+	 */
+	void exitAsignacion(PreciseV5Parser.AsignacionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PreciseV5Parser#varcte}.
+	 * @param ctx the parse tree
+	 */
+	void enterVarcte(PreciseV5Parser.VarcteContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PreciseV5Parser#varcte}.
+	 * @param ctx the parse tree
+	 */
+	void exitVarcte(PreciseV5Parser.VarcteContext ctx);
+}
