c2d437a6149a830ef8400472c0d39f3bd331c3d0
Miguel Bazán <miguelb1995@icloud.com>
2019-04-16 16:56:28 -0500

    Ahi va

--- PreciseV5/PreciseV5/Compiler/Heart.swift
+++ PreciseV5/PreciseV5/Compiler/Heart.swift
@@ -26,7 +26,21 @@ class Heart {
     
     let globalFunc = "global"
     
-    // DIRECTORIO DE PROCECIMIENTOS DONDE GUARDAMOS TODO NO SE TE OLVIDE WEY
+    var functions = [String:Funcion]()
+    
+    
+    
+    //   MARK: - Constantes memoria
+    
+    let constantBaseAddress = 3000
+    let globalBaseAddress = 6000
+    let localBaseAddress = 9000
+    let tempBaseAdress = 12000
+    let tempGlobalAddress = 15000
+    
+    
+    
+// DIRECTORIO DE PROCECIMIENTOS DONDE GUARDAMOS TODO NO SE TE OLVIDE WEY
     var DirectorioP = [Variables]()
     //     IMPORTANTE!!!!!!!
     
@@ -42,6 +56,22 @@ class Heart {
     
     /// PilaOperandos
     var operands = Stack<Int>()
+    
+    
+    
+    //    MARK: - Variables de memoria
+    
+    var constantMemory: Memory!
+    var globalMemory: Memory!
+    var localMemory: Memory!
+    var tempMemory: Memory!
+    
+    var tempGlobalMemory: Memory!
+    var tempLocalMemory: Memory!
+    var tempTempMemory: Memory!
+    
+    
+    
     
 //    var viewCont = ViewController!
 
@@ -76,8 +106,6 @@ class Heart {
     }
     
     
-    
-    
     func runCode(input: String){
         
         do {
@@ -89,28 +117,11 @@ class Heart {
             let extractor = PreciseV5Walker.init()
             try walker.walk(extractor, tree)
         } catch {
-//            print("Parse error: \(error.localizedDescription)")// Poner compile error
+           print("Parse error: \(error.localizedDescription)")  // Poner compile error
         }
     }
     
-//    func runCode(input: String, vc: ViewController){
-////        clearModels()
-//
-//        viewCont = vc
-//
-//        do {
-//            let lexer = PreciseV5Lexer(ANTLRInputStream(input))
-//            let tokens = CommonTokenStream(lexer)
-//            let parser = try PreciseV5Parser(tokens)
-//            let tree = try parser.preciseV5()
-//
-//            let walker = ParseTreeWalker()
-//            let extractor = PreciseV5Walker.init()
-//            try walker.walk(extractor, tree)
-//        } catch {
-//            print("Parse error: \(error.localizedDescription)")// Poner compile error
-//        }
-//    }
+
     
     // MARK: - Manage Quadruples
     
@@ -194,10 +205,167 @@ class Heart {
         return (functionCtx.VOID() != nil) ? .Void : getType(from: functionCtx.type().last!)
     }
     
+    func getTempAddress(forType type: Tipo) -> Int {
+        if isGlobal {
+            return tempGlobalMemory.save(type)
+        } else {
+            return tempMemory.save(type)
+        }
+    }
+    
+    func addExprQuad(){
+        let (opR, typeR) = getOperandAndType()
+        let (opL, typeL) = getOperandAndType()
+        let oper = operators.pop()!
+        
+        let resultType = getResultType(typeL, typeR, oper)
+        
+        if resultType != .Error {
+            let resultAddress = getTempAddress(forType: resultType)
+            addQuad(oper, opL, opR, resultAddress)
+            addOperandToStacks(address: resultAddress, type: resultType)
+        } else {
+            print("Type mismatch")
+        }
+    
+    }
 }
 
+// MARK: - Memory handling functions
+extension Heart {
+    
+    func resetLocalMemory() {
+        localMemory.reset()
+        tempMemory.reset()
+    }
+    
+    func getParamType(from funcName: String, paramNum: Int) -> Tipo {
+        if let type = functions[funcName]?.paramsSecuence[paramNum-1] {
+            return type
+        }
+        return .Error
+    }
+    
+    func getFuncAddress(with funcName: String) -> Int {
+        if let address = functions[funcName]?.address {
+            return address
+        }
+        return -1
+    }
+    
+//    func save(_ value: Any, in address: Int) {
+//        switch address {
+//        case ..<0:
+//            let (arrayAddress,_) = getValue(from: -address)
+//            //            return getValue(from: arrayAddress as! Address)
+//            save(value, in: arrayAddress as! Int)
+//        case ..<constantBaseAddress:
+//            let function = getFuncWithAddress(address)
+//            let funcName = getFuncName(of: function)
+//            let globalReturnVar = functions[funcName]?.variables[funcName]
+//            let globalReturnAddress = (globalReturnVar?.address)!
+//            save(value, in: globalReturnAddress)
+//        case ..<globalBaseAddress:
+//            constantsMemory.save(value, in: address)
+//        case ..<localBaseAddress:
+//            globalMemory.save(value, in: address)
+//        case ..<tempBaseAddress:
+//            localMemory.save(value, in: address)
+//        case ..<tempGlobalAddress:
+//            tempMemory.save(value, in: address)
+//        default:
+//            tempGlobalMemory.save(value, in: address)
+//        }
+//    }
+    
+//    func getFuncName(of function: Function) -> String {
+//        return functions.someKey(forValue: function)!
+//    }
+    
+//    func getValue(from address: Address) -> (value: Any, type: Type) {
+//
+//        switch address {
+//        case ..<0:
+//            let (arrayAddress,_) = getValue(from: -address)
+//            return getValue(from: arrayAddress as! Address)
+//        case ..<constantsBaseAddress:
+//            let function = getFuncWithAddress(address)
+//            let funcName = getFuncName(of: function)
+//            let globalReturnVar = functions[globalFunc]?.variables[funcName]
+//            let globalReturnAddress = (globalReturnVar?.address)!
+//
+//            return getValue(from: globalReturnAddress)
+//        case ..<globalBaseAddress:
+//            return constantsMemory.getValue(from: address)
+//        case ..<localBaseAddress:
+//            return globalMemory.getValue(from: address)
+//        case ..<tempBaseAddress:
+//            return localMemory.getValue(from: address)
+//        case ..<tempGlobalAddress:
+//            return tempMemory.getValue(from: address)
+//        default:
+//            return tempGlobalMemory.getValue(from: address)
+//        }
+//    }
 
-
+    
+    func getVariable(withId id: String) -> Variables? {
+        if let idVar = functions[currentFunction]?.variables[id] {
+            return idVar
+        } else if let idVar = functions[globalFunc]?.variables[id] {
+            return idVar
+        } else {
+//            compileError("Variable '\(id)' does not exists")
+            print("Variable '\(id)' does not exists")
+            return nil
+        }
+        
+    }
+    
+    func printQuads() {
+        
+        func formatNumber(_ string: String) -> String{
+            var str = string
+            let length = str.count
+            let new = 6 - length
+            
+            for _ in 0..<new {
+                str.insert(" ", at: str.startIndex)
+            }
+            
+            return str
+        }
+        
+        print("#  Operators   Left   Right  Temp  ")
+        print("-----------------------------------")
+        for index in 0..<quadsCount {
+            let quad = cuadruplos[index]
+            
+            var indexString = "\(index)"
+            var op = "\(quad.op)"
+            var left = quad.operandLeft?.description ?? "_____"
+            var right = quad.operandRight?.description ?? "_____"
+            var temp = quad.temp?.description ?? "_____"
+            
+            let length = op.count
+            if length != 11 {
+                let new = 11 - length
+                for _ in 1...new {
+                    op += " "
+                }
+            }
+            
+            let newIndexString = indexString + " "
+            indexString = indexString.count == 1 ? newIndexString : indexString
+            left = formatNumber(left)
+            right = formatNumber(right)
+            temp = formatNumber(temp)
+            
+            print(indexString, op, left, right, temp)
+            
+        }
+    }
+}
 
 extension Heart {
     
@@ -205,18 +373,23 @@ extension Heart {
     
     func enterPreciseV5(_ ctx:PreciseV5Parser.PreciseV5Context){
 
-        
+        if stop {return}
     }
     
     func exitPreciseV5(_ ctx:PreciseV5Parser.PreciseV5Context){
+        
+        if stop {return}
         print(DirectorioP)
     }
     
     func enterDeclare(_ ctx:PreciseV5Parser.DeclareContext){
     
+        if stop {return}
     }
     
     func exitDeclare(_ ctx:PreciseV5Parser.DeclareContext){
+        
+        if stop {return}
         
         let nombreVar = ctx.ID()!.getText()
         let varType = getType(from: ctx.type()!)
@@ -230,130 +403,132 @@ extension Heart {
     }
     
     func enterType(_ ctx:PreciseV5Parser.TypeContext){
-        
+        if stop {return}
     }
     
     func exitType(_ ctx:PreciseV5Parser.TypeContext){
-        
+        if stop {return}
     }
     
     func enterArray(_ ctx:PreciseV5Parser.ArrayContext){
-        
+        if stop {return}
     }
     
     func exitArray(_ ctx:PreciseV5Parser.ArrayContext){
-        
+        if stop {return}
     }
     
     func enterBody(_ ctx:PreciseV5Parser.BodyContext){
-        
+        if stop {return}
     }
     
     func exitBody(_ ctx:PreciseV5Parser.BodyContext){
-        
+        if stop {return}
     }
     
     func enterEstatuto(_ ctx:PreciseV5Parser.EstatutoContext){
-        
+        if stop {return}
     }
     
     func exitEstatuto(_ ctx:PreciseV5Parser.EstatutoContext){
-        
+        if stop {return}
     }
     
     func enterExpresionbool(_ ctx:PreciseV5Parser.ExpresionboolContext){
-        
+        if stop {return}
     }
     
     func exitExpresionbool(_ ctx:PreciseV5Parser.ExpresionboolContext){
-        
+        if stop {return}
     }
     
     func enterFunction(_ ctx:PreciseV5Parser.FunctionContext){
         
+        if stop {return}
+        
         let nombreFuncion = ctx.ID().first!
         
-//        print(nombreFuncion)
+        print(nombreFuncion)
         
         currentFunction =  ctx.ID().first!.getText()
     }
     
     func exitFunction(_ ctx:PreciseV5Parser.FunctionContext){
-        
+        if stop {return}
     }
     
     func enterExpresion(_ ctx:PreciseV5Parser.ExpresionContext){
-        
+        if stop {return}
     }
     
     func exitExpresion(_ ctx:PreciseV5Parser.ExpresionContext){
-        
+        if stop {return}
     }
     
     func enterExp(_ ctx:PreciseV5Parser.ExpContext){
-        
+        if stop {return}
     }
     
     func exitExp(_ ctx:PreciseV5Parser.ExpContext){
-        
+        if stop {return}
     }
     
     func enterTermino(_ ctx:PreciseV5Parser.TerminoContext){
-        
+        if stop {return}
     }
     
     func exitTermino(_ ctx:PreciseV5Parser.TerminoContext){
-        
+        if stop {return}
     }
     
     func enterFactor(_ ctx:PreciseV5Parser.FactorContext){
-        
+        if stop {return}
     }
     
     func exitFactor(_ ctx:PreciseV5Parser.FactorContext){
-        
+        if stop {return}
     }
     
     func enterCondicion(_ ctx:PreciseV5Parser.CondicionContext){
-        
+        if stop {return}
     }
     
     func exitCondicion(_ ctx:PreciseV5Parser.CondicionContext){
-        
+        if stop {return}
     }
     
     func enterCiclo(_ ctx:PreciseV5Parser.CicloContext){
-        
+        if stop {return}
     }
     
     func exitCiclo(_ ctx:PreciseV5Parser.CicloContext){
-        
+        if stop {return}
     }
     
     func enterEscritura(_ ctx:PreciseV5Parser.EscrituraContext){
-        
+        if stop {return}
     }
     
     func exitEscritura(_ ctx:PreciseV5Parser.EscrituraContext){
-        
+        if stop {return}
     }
     
     func enterLectura(_ ctx:PreciseV5Parser.LecturaContext){
         
-        
+        if stop {return}
     }
     
     func exitLectura(_ ctx:PreciseV5Parser.LecturaContext){
-        
+        if stop {return}
     }
     
     func enterAsignacion(_ ctx:PreciseV5Parser.AsignacionContext){
-        
+        if stop {return}
     }
     
     func exitAsignacion(_ ctx:PreciseV5Parser.AsignacionContext){
         
-        
+        if stop {return}
         
         
         
@@ -361,13 +536,21 @@ extension Heart {
     }
     
     func enterVarcte(_ ctx:PreciseV5Parser.VarcteContext){
-        
+        if stop {return}
     }
     
     func exitVarcte(_ ctx:PreciseV5Parser.VarcteContext){
-      
+        if stop {return}
     }
     
+    
+    
+    
+    
+    
+    
+    
+    // MARK: - Nodes and rules
     func enterEveryRule(_ ctx:ParserRuleContext){
         
     }
